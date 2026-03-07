608b99624d3eaab42201c155747c27f63f2d4ca4
fathinatiqah <fathinatiqahf@gmail.com>
2026-03-07 14:35:27 +0700

    feat : Praktikum 4

---
+++ Praktikum4/praktikum1.dart
@@ -0,0 +1,18 @@
+void main() {
+  /*var list = [1, 2, 3];
+  assert(list.length == 3);
+  assert(list[1] == 2);
+  print(list.length);
+  print(list[1]);
+
+  list[1] = 1;
+  assert(list[1] == 1);
+  print(list[1]);*/
+
+  final List<String?> data = List.filled(5, null);
+
+  data[1] = "Atiqah Fathin Fauziyyah";
+  data[2] = "244107060031";
+
+  print(data);
+}
